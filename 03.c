ad30a185cfcb181453ca7d725c7d50ddcf6049ee
cr.cardd <148579191+CrCardd@users.noreply.github.com>
2024-04-05 17:13:15 -0300

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    int bool = 0;
+    char name[];
+    FILE *not = fopen("agenda.txt","w");
+    while(bool){
+
+    }
+
+
+    return 0;
+}
